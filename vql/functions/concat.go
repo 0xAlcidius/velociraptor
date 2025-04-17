09f1b5c3bf37e143945cde62c807f3478234f85c
0xNerva <guus@nawijn.eu>
2025-04-17 10:37:08 +0200

    Create concat.go

---
+++ vql/functions/concat.go
@@ -0,0 +1,42 @@
+package functions
+
+import (
+	"context"
+
+	"github.com/Velocidex/ordereddict"
+	vql_subsystem "www.velocidex.com/golang/velociraptor/vql"
+	"www.velocidex.com/golang/vfilter"
+)
+
+// type ConcatFunctionArgs struct {
+// 	AlertName string    `vfilter:"required,field=name,doc=Name of the alert."`
+// 	DedupTime int64     `vfilter:"optional,field=dedup,doc=Suppress same message in this many seconds (default 7200 sec or 2 hours)."`
+// 	Condition types.Any `vfilter:"options,field=condition,doc=If specified we ignore the alert unless the condition is true"`
+// }
+
+type ConcatFunction struct{}
+
+func (self _ItemsFunc) Info(scope vfilter.Scope, type_map *vfilter.TypeMap) *vfilter.FunctionInfo {
+	return &vfilter.FunctionInfo{
+		Name:    "concat",
+		Doc:     "concatonates N strings together.",
+		Version: 1,
+	}
+}
+
+func (self *ConcatFunction) Call(ctx context.Context,
+	scope vfilter.Scope,
+	args *ordereddict.Dict) vfilter.Any {
+
+	new := vfilter.NewScope()
+	for _, k := range args.Keys() {
+		v, _ := args.Get(k)
+		new.Add(new, v)
+	}
+
+	return new
+}
+
+func init() {
+	vql_subsystem.RegisterFunction(&_ItemsFunc{})
+}
