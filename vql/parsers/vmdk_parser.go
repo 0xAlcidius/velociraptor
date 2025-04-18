6d408ecc50f8124ef529de4c1c826796fc2bc474
0xNerva <guus@nawijn.eu>
2025-04-18 17:20:04 +0200

    Update vmdk_parser.go

--- vql/parsers/vmdk_parser.go
+++ vql/parsers/vmdk_parser.go
@@ -121,7 +121,7 @@ func (self VmdkParser) Call(ctx context.Context,
 
 			fmt.Println("[VMDK_PARSER] File opened: ", filename.String())
 
-			buf := make([]byte, SectorSize)
+			buf := make([]byte, 8)
 			n, err := fd.Read(buf)
 
 			fmt.Println("[VMDK_PARSER] Contents: ", string(buf))
